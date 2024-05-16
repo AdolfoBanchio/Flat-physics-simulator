1f4a0494d406259c30c68f58a05f32e60a26e6af
AdolfoBanchio <adolfo.banchio@mi.unc.edu.ar>
2024-05-16 20:11:42 -0300

    Timesteps funcionando, funcion boba para mover formas

--- main.cpp
+++ main.cpp
@@ -14,35 +14,73 @@ I will use SFML library to draw the shapes and simulate the physics.
 #include "collision.h"
  
 // First star creating the main function and creating some windows and shapes
+std::vector<sf::Vector2f> corners = {
+    sf::Vector2f(100, 100),
+    sf::Vector2f(200, 100),
+    sf::Vector2f(250, 150),
+    sf::Vector2f(200, 200),
+    sf::Vector2f(100, 200)
+};
+Polygon polygon(corners);
+// create a triangle
+std::vector<sf::Vector2f> corners2 = {
+    sf::Vector2f(300, 300),
+    sf::Vector2f(400, 300),
+    sf::Vector2f(350, 400)
+};
+Polygon polygon2(corners2, sf::Color::Green);
+// see if the polygons are colliding
+//create a Mainfold with the shapes
+Mainfold coll = Mainfold(&polygon, &polygon2);
+
+const float fps = 5.0f;
+const float dt = 1.0f / fps;
+sf::Time dtTime = sf::seconds(dt);
+sf::Time accumulatorTime = sf::seconds(0.0f);
+
+float factor = 1.01;
+void updatePositions(Mainfold&coll,sf::RenderWindow& window){
+    
+    Polygon *polygon = dynamic_cast<Polygon*>(coll.shapeA);
+    Polygon *polygon2 = dynamic_cast<Polygon*>(coll.shapeB);
+    bool cond = true;
+    if (coll.isColliding) {
+            polygon->setColor(sf::Color::Blue);
+            sf::VertexArray normalLine(sf::LinesStrip, 2);
+            normalLine[0].position = polygon->mCenter;
+            normalLine[0].color = sf::Color::Red; // Set normal line start color (black)
+            normalLine[1].position = polygon->mCenter + 1000.0f * coll.normal; // Extend normal line by 20 units
+            normalLine[1].color = sf::Color::Red; // Set normal line end color (red)
+            // print in the stout the normal of the collision
+            window.draw(normalLine);
+            cond = false;
+        }else{
+            polygon->setColor(sf::Color::White);
+        }
+        // end the current frame
+
+        // rescale the polygon gradualy to a factor of 2.5 depending on the time, 
+        //then scale it down to the original value
+        if(cond) {
+            //polygon.Scale(factor);
+            polygon2->Scale(factor);
+            factor += 0.01;
+            //polygon2.Translate(sf::Vector2f(0, -1));
+            polygon->Translate(sf::Vector2f(0.3, 1));
+        }
+}
+
+
 int main()
 {
     // create a window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Physics 2D Simulator");
 
-    // create a pentagon
-    std::vector<sf::Vector2f> corners = {
-        sf::Vector2f(100, 100),
-        sf::Vector2f(200, 100),
-        sf::Vector2f(250, 150),
-        sf::Vector2f(200, 200),
-        sf::Vector2f(100, 200)
-    };
-    Polygon polygon(corners);
+    // create a clock to keep track of the time
+    sf::Clock clock;
+    sf::Time frameStart = clock.getElapsedTime();
 
-    // create a triangle
-    std::vector<sf::Vector2f> corners2 = {
-        sf::Vector2f(300, 300),
-        sf::Vector2f(400, 300),
-        sf::Vector2f(350, 400)
-    };
-    Polygon polygon2(corners2, sf::Color::Green);
-
-    // see if the polygons are colliding
-    //create a Mainfold with the shapes
-    Mainfold coll = Mainfold(&polygon, &polygon2);
-    PolyvsPoly(polygon, polygon2, coll);
-    
-    // print the polygon corners of the polygon and normals in the stdout
+    std::cout << dtTime.asSeconds() << std::endl;
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -54,8 +92,26 @@ int main()
                 window.close();
         }
 
-        // clear the window with black color
         window.clear(sf::Color::Black);
+        // get the current time
+        sf::Time currentTime = clock.getElapsedTime();
+
+        // calculate the time since the last frame
+        accumulatorTime += currentTime - frameStart;
+        frameStart = currentTime;
+
+        //std::cout << accumulatorTime.asSeconds() << std::endl;
+        if (accumulatorTime.asSeconds() > 0.2f){
+            accumulatorTime = sf::seconds(0.2f);
+        }
+
+        while(sf::operator>=(accumulatorTime, dtTime)){
+            // update the physics
+            updatePositions(coll,window);
+            // update the time
+            accumulatorTime -= dtTime;
+        }
+        // clear the window with black color
 
         // render the polygon
         polygon.render(window);
@@ -69,43 +125,8 @@ int main()
 
         PolyvsPoly(polygon, polygon2, coll);
 
-        bool cond = true;
-        //render the normal of the collition if there is one
-
         window.display();
-        
-        if (coll.isColliding) {
-            polygon.setColor(sf::Color::Blue);
-            sf::VertexArray normalLine(sf::LinesStrip, 2);
-            normalLine[0].position = polygon.mCenter;
-            normalLine[0].color = sf::Color::Red; // Set normal line start color (black)
-            normalLine[1].position = polygon.mCenter + 1000.0f * coll.normal; // Extend normal line by 20 units
-            normalLine[1].color = sf::Color::Red; // Set normal line end color (red)
-            // print in the stout the normal of the collision
-            window.draw(normalLine);
-            cond = false;
-        }else{
-            polygon.setColor(sf::Color::White);
-        }
-        
-        // end the current frame
-
-        // rescale the polygon gradualy to a factor of 2.5 depending on the time, 
-        //then scale it down to the original value
-        if(cond) {
-            float factor = 1.01;
-            //polygon.Scale(factor);
-            polygon2.Scale(factor);
-
-            polygon2.render(window);
-            factor += 0.01;
-
-            //polygon2.Translate(sf::Vector2f(0, -1));
-            polygon.Translate(sf::Vector2f(0.3, 1));
-        }
-
-        // wait 1 second for the next loop
-        sf::sleep(sf::seconds(0.2f));   
+        // sf::sleep(sf::milliseconds(1000/fps));
     }
 
 }
